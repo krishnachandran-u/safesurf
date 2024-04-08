c573085c00fbbfca7803ff13864767a7df0cf7f0
Krishnachandran U <kriss52450@gmail.com>
2024-04-08 21:37:31 +0530

    make api requests work

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', getCurrentTab);
+// document.addEventListener('DOMContentLoaded', getContent("https://github.com/"));
 
 async function getCurrentTab() {
   try {
@@ -6,7 +7,28 @@ async function getCurrentTab() {
     let [tab] = await chrome.tabs.query(queryOptions);
     const urlDiv = document.getElementById('url');
     urlDiv.textContent = tab.url;
+
+    //const content = getContent(tab.url);
+    const content = await getContent(tab.url);
+    const contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = "Content: " + content;
+
   } catch (error) {
     console.error("Error retrieving tab URL:", error);
   }
 }
+
+async function getContent(url) {
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error ${response.status}`);
+        }
+        return response.text();
+      })
+      .then(data => data)
+      .catch(error => {
+        console.error('Error:', error);
+        return null;
+      });
+  }
